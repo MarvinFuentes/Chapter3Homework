aa451f319096c984d742c1b2669dd5399b52d4db
marvi <mfuentes1@ggc.edu>
2023-02-09 23:07:13 -0500

    final

--- src/BMI.java
+++ src/BMI.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class BMI {
+/**
+ * @class: TrianglePerimeter
+ * @aurthor: Marvin Fuentes
+ * @course: ITEC2140 section4
+ * @written: February 9, 2023
+ * This program will determine if you are underweight, normal, overweight, or obese
+ * by entering your height in inches and weight in pounds. The program will automatically
+ * convert lbs to kgs and inches to meters.
+ */
+import java.util.Scanner;
+public class BMI {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        // Read the weight and height of user.
+        System.out.print("Enter your weight in pounds:");
+        // Prompt to enter weight.
+        double lbs = input.nextDouble(); // Input as a decimal.
+        System.out.print("Enter your height in inches:");
+        // Prompt to enter height.
+        double height = input.nextDouble(); // Input as a decimal.
+        double kg = (lbs * 0.4536);
+        // Conversion formula for pounds to kilograms.
+        double meters = (height * 0.0254);
+        // Conversion formula for inches to meters.
+        double bmi = (kg / (meters * meters)); // Formula for BMI.
+        if (bmi < 18.5){ // First if statement to determine if user is underweight.
+            System.out.print("Underweight");
+            // Print out underweight if statement is true.
+        }
+        else if (18.5 <= bmi && bmi < 25.0){
+            // Else if to determine if user is normal and not continuing if statements.
+            System.out.print("Normal"); // Print out Normal if statement is true.
+        }
+        else if (25.0 <= bmi && bmi < 30.0){
+            // Else if to determine if user is overweight and not continuing if statements.
+            System.out.print("Overweight"); // Print out overweight if statement is true
+        }
+        else { // Else statement to lastly determine if the user is obese.
+            System.out.println("Obese"); // Print out obese if statement is true.
+        }
+    }
 }
--- src/TrianglePerimeter.java
+++ src/TrianglePerimeter.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class TrianglePerimeter {
+/**
+ * @class: TrianglePerimeter
+ * @aurthor: Marvin Fuentes
+ * @course: ITEC2140 section4
+ * @written: February 9, 2023
+ * This program will find the perimeter of any triangle that is
+ * reasonable nonetheless the program will prompt you with invalid input.
+ */
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class TrianglePerimeter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Read all sides of the triangle.
+        System.out.print("Enter length of side a of Triangle:");
+        // Prompt to enter dimension for side a.
+        int a = input.nextInt();
+        // Input as an integer.
+        System.out.print("Enter length of side b of Triangle:");
+        // Prompt to enter dimension for side b.
+        int b = input.nextInt();
+        // Input as an integer.
+        System.out.print("Enter length of side c of triangle:");
+        // Prompt to enter dimension for side c.
+        int c = input.nextInt();
+        // Input as an integer.
+        if ( a >= b + c || b >= a + c || c >= b + a) {
+            // Declaring an if statement to determine id the triangle is valid.
+            System.out.print("Invalid input");
+            // Print out "Invalid input" if statement is true.
+        }
+        else {
+            // Else statement if statement isn't true.
+            int p = (a + b + c);
+            // Formula for finding the perimeter of a triangle.
+            System.out.print("Perimeter of triangle:" + p);
+            // Print out "perimeter of triangle" plus the perimeter of the triangle.
+        }
+    }
 }
